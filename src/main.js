ad93eb6d3c34a9597cff7e6ecc1715a857972f15
qyhever <15927700473@1631.com>
2022-04-23 23:07:27 +0800

    import nanoid

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import './assets/styles/index.scss'
-import './assets/icons'
+// import './assets/icons'
 import './plugins'
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
