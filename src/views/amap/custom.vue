fae28b3ddcb60f0ff5d942ed2abd72559b82a729
yohqin <15927700473@163.com>
2024-04-15 18:21:35 +0800

    update custom

--- src/views/amap/custom.vue
+++ src/views/amap/custom.vue
@@ -5,23 +5,31 @@
 </template>
 
 <script>
-import { random } from 'lodash'
+import { random, subtract, add, divide, flattenDeep, max, min } from 'lodash'
 import asyncLoadScript from '@/utils/async-script'
-import jsonData from '@/data/gongan/data.json'
-// import jsonData from '@/data/songzi/data.json'
+import songziJsonData from '@/data/songzi/data.json'
+import gonganJsonData from '@/data/gongan/data.json'
 
 const mapPlugins = [
   'CitySearch',
   'Scale',
   'ToolBar',
-  'PlaceSearch',
-  'InfoWindow',
-  'DistrictSearch'
+  'PlaceSearch'
 ]
 const mapPluginsStr = mapPlugins.map(item => 'AMap.' + item).join(',')
 const url = 'https://webapi.amap.com/maps?v=2.0&key=6a169cffad64fb2322801c076ae7d3ec&plugin=' + mapPluginsStr
 
 let polygons = []
+let infoWindow = null
+
+function generateInfoWindow (content) {
+  infoWindow = new window.AMap.InfoWindow({
+    content: '<div style="font-size: 12px">' + content + '</div>',
+    isCustom: true,
+    autoMove: false
+  })
+}
+
 export default {
   name: 'AMapPage',
   data () {
@@ -71,23 +79,69 @@ export default {
       const AMap = window.AMap
       const map = this.map
       // map.setZoomAndCenter(9, result.districtList[0].center)
-      map.setZoomAndCenter(9, [
-        111.60851085684304,
-        30.17389903190419
-      ])
-      console.log('jsonData', jsonData)
-      jsonData.forEach(item => {
-        const fillOpacity = Number(String(random(0.2, 0.9)).slice(0, 3))
-        console.log('name', item.name)
-        const polygon = new AMap.Polygon({
-          strokeWeight: 1,
-          path: item.coordinates,
-          fillOpacity,
-          fillColor: '#80d8ff',
-          strokeColor: '#0091ea'
+      // map.setZoomAndCenter(9, [
+      //   111.60851085684304,
+      //   30.17389903190419
+      // ])
+      function generatePolygon (jsonData, fillColor, strokeColor) {
+        jsonData.forEach(item => {
+          const fillOpacity = Number(String(random(0.1, 0.8)).slice(0, 3))
+          item.coordinates.forEach(path => {
+            const flattenedCoordinates = flattenDeep(path) // [lng1, lat1, lng2, lat2, ...]
+            const lngs = []
+            const lats = []
+            flattenedCoordinates.forEach((v, i) => {
+              if (i % 2 === 0) {
+                lngs.push(v)
+              } else {
+                lats.push(v)
+              }
+            })
+            const centerLng = divide(
+              max(lngs) + min(lngs),
+              2
+            )
+            const centerLat = divide(
+              max(lats) + min(lats),
+              2
+            )
+            const polygon = new AMap.Polygon({
+              strokeWeight: 1,
+              path,
+              fillOpacity,
+              fillColor: fillColor || '#80d8ff',
+              strokeColor: strokeColor || '#0091ea',
+              extData: {
+                name: item.name.replace('.json', ''),
+                fillOpacity,
+                centerPosition: [centerLng, centerLat]
+              }
+            })
+            polygon.on('mouseover', () => {
+              const options = polygon.getOptions()
+              const extData = polygon.getExtData()
+              polygon.setOptions({
+                fillOpacity: add(options.fillOpacity, 0.05)
+              })
+              if (!infoWindow) {
+                generateInfoWindow(extData.name)
+                infoWindow && infoWindow.open(map, extData.centerPosition)
+              }
+            })
+            polygon.on('mouseout', () => {
+              const options = polygon.getOptions()
+              polygon.setOptions({
+                fillOpacity: subtract(options.fillOpacity, 0.05)
+              })
+              infoWindow && infoWindow.close()
+              infoWindow = null
+            })
+            polygons.push(polygon)
+          })
         })
-        polygons.push(polygon)
-      })
+      }
+      generatePolygon(songziJsonData, '#ccebc5', '#2b8cbe')
+      generatePolygon(gonganJsonData)
       map.add(polygons)
       map.setFitView(polygons)
     },
@@ -108,16 +162,7 @@ export default {
   width: 100%;
   height: 100%;
 }
-.city-input {
-  z-index: 1;
-  position: absolute;
-  top: 20px;
-  left: 20px;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 4px;
-}
-.clear-button {
-  margin-left: 20px;
+.amap-info {
+  pointer-events: none !important;
 }
 </style>
